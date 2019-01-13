3f6d4a2b704531b20f145ed1a2df71c78e7c5a53
Kumazaki <white.march322@gmail.com>
2019-01-13 21:56:29 +0900

    vuemoov

---
+++ app/assets/javascripts/noodles.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#new',
+  data:{
+    preview: ''
+  },
+  methods: {
+    handlecahnge: function(event){
+      var file = event.target.files[0]
+      if (file && file.type.match(/^image\/(png|jpeg)$/)){
+        this.preview = window.URL.createObjectURL(file)
+      }
+    }
+  }
+})
