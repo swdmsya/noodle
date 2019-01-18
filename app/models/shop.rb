8aaf6f3ec773c1f6fc3e8a5e4b027989dae83d63
Kumazaki <white.march322@gmail.com>
2019-01-18 13:39:21 +0900

    投稿できることコンソールから確認

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,9 @@ class Shop < ApplicationRecord
   #associations
   has_many :shops
   has_many :users, through: :shops
+
+  #validation
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :url, presence: true
 end
--- db/migrate/20190118030209_create_posts.rb
+++ db/migrate/20190118030209_create_posts.rb
@@ -2,13 +2,13 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text    :impressions
-      t.integer :genre_id, null: false
-      t.integer :user_id, null: false
-      t.integer :store_id
+      t.integer :genre_id,  default: 10
+      t.integer :user_id
+      t.integer :shop_id
       t.timestamps
     end
     add_index :posts, :genre_id            
-    add_index :posts, :store_id
+    add_index :posts, :shop_id
     add_index :posts, :user_id
   end
 end
