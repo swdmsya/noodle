b8d55341ef9732f15fa5ff8c0e6f76ccce7b563a
Kumazaki <white.march322@gmail.com>
2019-02-05 13:42:22 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,41 @@ genre.each.with_index(1) do |item, i|
 end
 
 
+user = User.new(nickname:  "ラーメン太郎",
+  email: "test@test.com",
+  password:              "11111111",
+  password_confirmation: "11111111",)
+user.avatar.attach(io: File.open("#{Rails.root}/app/assets/images/ginnokura.png"),
+              filename: "ginnnokura.png", content_type: "image/png")
+
+user.save
+
+25.times do |n|
+  name = Faker::Pokemon.name
+  password = "password"
+  user = User.new(nickname: name,
+               email: "test#{n}@gmail.com",
+               password: password,
+               password_confirmation: password,
+             )
+  user.avatar.attach(io: File.open("#{Rails.root}/app/assets/images/jirou.png"),
+  filename: "jirou.png", content_type: "image/png")
+  user.save
+end
+
+users = User.all
+user  = users.first
+following = users[2..9]
+followers = users[3..15]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+
+5.times do
+  content = Faker::Lorem.sentence(5)
+  users.each do |user|
+    post = user.posts.new(impressions: content,genre_id: 2)
+    post.image.attach(io: File.open("#{Rails.root}/app/assets/images/jirou.png"),
+    filename: "jirou.png", content_type: "image/png")
+  end
+end
