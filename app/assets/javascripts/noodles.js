a53186b03b85259af28c3887169ca4ce33d2c0e8
Sawada <swdmsya@icloud.com>
2019-02-20 16:29:13 +0900

    プレビュー

--- app/assets/javascripts/noodles.js
+++ app/assets/javascripts/noodles.js
@@ -1,14 +1,14 @@
-var app = new Vue({
-  el: '#new',
-  data:{
-    preview: '',
-  },
-  methods: {
-    handlechange: function(event){
-      var file = event.target.files[0]
-      if (file && file.type.match(/^image\/(png|jpeg)$/)){
-        this.preview = window.URL.createObjectURL(file)
-      }
-    }
-  }
-})
+// var app = new Vue({
+//   el: '#post_new',
+//   data:{
+//     preview: '',
+//   },
+//   methods: {
+//     handlechange: function(event){
+//       var file = event.target.files[0]
+//       if (file && file.type.match(/^image\/(png|jpeg)$/)){
+//         this.preview = window.URL.createObjectURL(file)
+//       }
+//     }
+//   }
+// })
